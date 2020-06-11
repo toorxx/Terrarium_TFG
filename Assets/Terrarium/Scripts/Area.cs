f5998bc78c5419d7cb5433de105578fb759b1df9
gecaro <gerard.camps01@estudiant.upf.edu>
2020-06-11 14:08:34 +0200

    bug fixing + refactor

--- Assets/Terrarium/Scripts/Area.cs
+++ Assets/Terrarium/Scripts/Area.cs
@@ -10,6 +10,8 @@ public class Area : MonoBehaviour
     public int _min_plants;
     public int _time_before_herb;
     public int _time_before_carn;
+    public int _time_before_agents_deactivation;
+    private bool agentsDeactivated;
     public List<GameObject> Plants;
     public List<GameObject> Herbivores;
     public List<GameObject> Carnivores;
@@ -27,6 +29,7 @@ public class Area : MonoBehaviour
         Herbivores = new List<GameObject>();
         Carnivores = new List<GameObject>();
         controlledGO = new GameObject();
+        agentsDeactivated = false;
         for(int i = 0; i < _initial_plants; i++)
         {
             Instantiate(plantPrefab, GetRandomPos(), Quaternion.identity, transform);
@@ -36,36 +39,23 @@ public class Area : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Debug.Log(Time.time);
         // add plants randomly at x steps of time
         if (Time.frameCount % 1 == 0)
         {
             if(Plants.Count < _min_plants)
-            {
                 Instantiate(plantPrefab, GetRandomPos(), Quaternion.identity, transform);
-            }
         }
         if (Time.frameCount == _time_before_herb)
-        {
-            for (int i = 0; i < _initial_herbivores; i++)
-            {
-                Instantiate(herbivorePrefab, GetRandomPos(), Quaternion.identity, transform);
-            }
-        }
+            setAgentsList("herbivore");
         if (Time.frameCount == _time_before_carn)
-        { 
-            for (int i = 0; i < _initial_herbivores; i++)
-            {
-                Instantiate(herbivorePrefab, GetRandomPos(), Quaternion.identity, transform);
-            }
-        }
-
-        if (Time.frameCount > 100)
+            setAgentsList("carnivore");
+        if (Time.frameCount > _time_before_agents_deactivation && !agentsDeactivated)
         {
-            foreach (var herbivore in Herbivores)
-            {
-                herbivore.GetComponent<CreatureAgent>().canDisappear = true;
-            }
+            SetDeactivation();
+            agentsDeactivated = true;
         }
+        
 
     }
 
@@ -92,8 +82,8 @@ public class Area : MonoBehaviour
         else if (go.tag == "carnivore")
             Carnivores.Add(go);
     }
-
-    public Vector2 GetRandomPos()
+s
+    public Vector3 GetRandomPos()
     {
         var bounds = GetBounds();
         var x = Random.Range(-bounds.x, bounds.x);
@@ -107,6 +97,29 @@ public class Area : MonoBehaviour
         MLAgents.Monitor.Log("Num Plants", Plants.Count, transform);
         MLAgents.Monitor.Log("Num herbivores", Herbivores.Count, transform);
         MLAgents.Monitor.Log("Num carnivores", Carnivores.Count, transform);
+    }
+
+    // set the agents to deactivate. We use this in order to not instantiate agents when episode is finished
+    private void SetDeactivation()
+    {
+        foreach(var herbivore in Herbivores)
+            herbivore.GetComponent<Herbivore>().canDisappear = true;
+        foreach(var carnivore in Carnivores)
+            carnivore.GetComponent<Carnivore>().canDisappear = true;
+    }
+
+    private void setAgentsList(string kind)
+    {
+        if(kind == "herbivore")
+        {
+            for (int i = 0; i < _initial_herbivores; i++)
+                 Instantiate(herbivorePrefab, GetRandomPos(), Quaternion.identity, transform);
+        }
+        else if(kind == "carnivore")
+        {
+            for (int i = 0; i < _initial_carnivores; i++)
+                Instantiate(carnivorePrefab, GetRandomPos(), Quaternion.identity, transform);
+        }
     }
 
 
